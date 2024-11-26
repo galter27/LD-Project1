15282b43390f360078bc4e96f7e7b35c683887d5
gternovs <gternovs@akamai.com>
2024-11-26 17:57:39 +0200

    Added ansible

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -1,20 +1,17 @@
+resource "aws_key_pair" "jenkins_ansible_key" {
+  key_name   = "jenkins-ansible-key"
+  public_key = file(var.public_key_path)  # Path to your public key
+
+  tags = {
+    Name = "Jenkins Ansible SSH Key"
+  }
+}
+
 resource "aws_instance" "jenkins-server" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  key_name      = var.private_key_name
+  key_name      = aws_key_pair.jenkins_ansible_key.key_name
   security_groups = [aws_security_group.jenkins_sg.name]
-
-  user_data = <<-EOF
-    #!/bin/bash
-    sudo apt update -y && sudo apt install fontconfig openjdk-17-jre -y
-    sudo wget -O /usr/share/keyrings/jenkins-keyring.asc https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-    echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-    sudo apt-get update -y && sudo apt-get install jenkins -y
-    sudo cat /var/lib/jenkins/secrets/initialAdminPassword > /home/ubuntu/jenkins_token.txt
-    sudo apt-get update  && sudo apt install docker.io -y && sudo systemctl start docker && sudo systemctl enable docker
-    sudo usermod -aG docker jenkins
-    sudo systemctl restart jenkins
-    EOF
 
   root_block_device {
     volume_size = 10
@@ -23,26 +20,6 @@ resource "aws_instance" "jenkins-server" {
 
   tags = {
     Name = "Jenkins Server"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      # Wait until the Jenkins token file exists
-      "while [ ! -f /home/ubuntu/jenkins_token.txt ]; do sleep 5; done",
-
-      # Print the Jenkins token to the Terraform logs
-      "echo '#################### JENKINS TOKEN ####################'",
-      "cat /home/ubuntu/jenkins_token.txt",
-      "echo '#################### JENKINS TOKEN ####################'"
-
-    ]
-
-    connection {
-      type        = "ssh"
-      user        = "ubuntu"
-      private_key = file(var.private_key_file)
-      host        = self.public_ip
-    }
   }
 
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,24 @@
-variable "private_key_name" {
-  description = "The name of the private SSH key to use for the instance"
+variable "public_key_path" {
+  description = "Path to the public key file"
   type        = string
+  default     = "~/.ssh/jenkins-ansible-key.pub"  # or use a relative path
 }
 
 variable "private_key_file" {
-  description = "The name of the private SSH key to use for the instance"
+  description = "Path to the private key file"
   type        = string
+  default     = "~/.ssh/jenkins-ansible-key"  # Private key used for connecting
 }
+
+# variable "private_key_name" {
+#   description = "The name of the private SSH key to use for the instance"
+#   type        = string
+# }
+
+# variable "private_key_file" {
+#   description = "The name of the private SSH key to use for the instance"
+#   type        = string
+# }
 
 variable "ami_id" {
   description = "The AMI ID for the Jenkins server"
